978a6303f1943fa9d31ae771a847b949cf417f63
kecarus <ken@ipl31.net>
2019-11-30 21:50:40 -0800

    whee

--- build_image.sh
+++ build_image.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 echo "Installing deps"
 #sudo apt-get install -y qemu-user-static debootstrap
+apt-get update -y
 apt-get install -y sudo
-sudo apt-get update -y
 sudo apt-get install -y debootstrap
 
 CHROOT_DIR=$(mktemp -d -t ci-XXXXXXXXXX)
